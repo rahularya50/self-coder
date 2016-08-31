e8a3f68368eb59d64e0b290e26071ba3f9fb8fac
rahularya50 <rahularya50@gmail.com>
2016-08-31 23:10:02 +0800

    Transitions now working

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
-for (index in sequence)
-{
-	css_submit(sequence[index])
-}
+var stylesheet_main = null;
 
+initialize();
 
-function css_submit(css)
+function initialize()
 {
     head = document.head || document.getElementsByTagName('head')[0],
-    style = document.createElement('style');
-		
-	$("*").animate({
-		fontSize: "72pt"
-	}, 500
-	)
+    stylesheet_main = document.createElement('style');
 	
-	
-	style.type = 'text/css';
+	stylesheet_main.type = 'text/css';
 
-	style.appendChild(document.createTextNode(css));
-			
-	//head.appendChild(style);
+	head.appendChild(stylesheet_main);
+}
+
+function css_submit()
+{	
+	update_css(document.getElementById("css_text_input").value);	
 	
 	return false;
+}
+
+function update_css(css)
+{
+	if (stylesheet_main.childNodes.length > 0)
+	{
+		//stylesheet_main.removeChild(stylesheet_main.childNodes[0]);
+	}
+	
+	stylesheet_main.appendChild(document.createTextNode(css));	
 }
--- site_program.js
+++ site_program.js
@@ -1,1 +1,5 @@
 var sequence = ["h1 {background-color: red;}"] //@keyframes example {from {background-color: red;} to {background-color: yellow;} } h1 {background-color: red; animation-name: example; animation-duration: 4s; }"]
+
+var final_css = `
+h1 {color: blue}
+`
